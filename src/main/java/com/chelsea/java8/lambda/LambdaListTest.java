9f59831c79d96b8db99a223f3a5c37fdb4296ece
baojun_a <baojun_a@aspirecn.com>
2019-11-05 11:00:15 +0800

    修改类名

---
+++ src/main/java/com/chelsea/java8/lambda/LambdaListTest.java
@@ -0,0 +1,30 @@
+package com.chelsea.java8.lambda;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Lambda list集合
+ * 
+ * @author shevchenko
+ *
+ */
+public class LambdaListTest {
+
+    public static void main(String[] args) {
+        // 循环遍历list
+        Arrays.asList("a", "b", "d").forEach(e -> System.out.println(e));
+        Arrays.asList("a", "b", "d").forEach(e -> {
+            System.out.print(e);
+            System.out.println(e);
+        });
+        String separator = ",";
+        Arrays.asList("a", "b", "d").forEach((String e) -> System.out.println(e + separator));
+        // list排序
+        List<Integer> list = Arrays.asList(3, 1, 4);
+        list.sort((e1, e2) -> e1.compareTo(e2));
+        System.out.println(list);
+        
+    }
+
+}
